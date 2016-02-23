9861324589123916960969995fcc945ed40abc0e
James Featherstone <jfeathe4@uwo.ca>
2016-02-23 12:44:20 -0500

    cleaned up files

---
+++ src/main/java/DashBoardPanel.java
@@ -0,0 +1,35 @@
+package team08_app;
+
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class DashBoardPanel extends JPanel {
+	private static final int WIDTH = 300;
+	private static final int HEIGHT = 150;
+
+	/**
+	 * creates a new panel for the daily dashboard
+	 * @param x the starting x position
+	 * @param y the starting y position
+	 */
+	public DashBoardPanel(int x, int y)
+	{
+		JPanel panel = new JPanel();
+		Rectangle r = new Rectangle(x, y, WIDTH, HEIGHT);
+		panel.setBounds(r);
+		
+	}
+	/* code that can be used to track for clicks
+	   panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			//what to do on button click here!
+			}
+		});
+	 */
+}
