97d5da8fdc962897c795d0380b24978e519db2f1
Ryan Zegray <rzegray@uwo.ca>
2016-02-24 23:42:58 -0500

    Removed an additional line from App.Java

--- src/main/java/ca/uwo/csd/cs2212/team08/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team08/App.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 public class App {
 
 	public static void main (String args[]){
-		System.out.println("Floors Climbed: " + test.getFloorsClimbed());
 		/*
 		 SwingUtilities.invokeLater(new Runnable() {
              @Override
