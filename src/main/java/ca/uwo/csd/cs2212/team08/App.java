8a268462efb4119a5d083c09b2747338bf996218
Ryan Zegray <rzegray@uwo.ca>
2016-02-24 23:40:53 -0500

    Removed lines from app.java to fix conflict on merge

--- src/main/java/ca/uwo/csd/cs2212/team08/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team08/App.java
@@ -4,8 +4,6 @@ import javax.swing.SwingUtilities;
 public class App {
 
 	public static void main (String args[]){
-		APIData test = new APIData();
-		test.refreshDashBoardData(0, 0, 0);
 		System.out.println("Floors Climbed: " + test.getFloorsClimbed());
 		/*
 		 SwingUtilities.invokeLater(new Runnable() {
