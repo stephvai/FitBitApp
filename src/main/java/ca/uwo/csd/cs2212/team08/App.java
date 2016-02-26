4895bb6f600799aa5388413d023d12704351ffdb
Ryan Zegray <rzegray@uwo.ca>
2016-02-26 15:15:26 -0500

    Changed app.java to fix a potential merge conflict

--- src/main/java/ca/uwo/csd/cs2212/team08/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team08/App.java
@@ -4,6 +4,9 @@ import javax.swing.SwingUtilities;
 public class App {
 
 	public static void main (String args[]){
+		System.out.println("Hello World");
+		MainScreen window = new MainScreen();
+		window.setVisible(true);
 		/*
 		 SwingUtilities.invokeLater(new Runnable() {
              @Override
