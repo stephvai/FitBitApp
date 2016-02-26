6015d3cad763e9493326e7e3d8a4c11ac2d8f6eb
James Featherstone <jfeathe4@uwo.ca>
2016-02-26 15:34:16 -0500

    added new panel classes

--- src/main/java/ca/uwo/csd/cs2212/team08/DistancePanel.java
+++ src/main/java/ca/uwo/csd/cs2212/team08/DistancePanel.java
@@ -2,34 +2,41 @@ package ca.uwo.csd.cs2212.team08;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Button;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JInternalFrame;
-import javax.swing.JTextPane;
 import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JTabbedPane;
 
-public class StepsPanel extends JFrame {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class DistancePanel extends JFrame {
 
 	private JPanel contentPane;
 	private static final String backImage = "src/main/resources/Placeholder.png";
 
 	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DistancePanel frame = new DistancePanel();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
 	 * Create the frame.
 	 */
-	public StepsPanel() {
+	public DistancePanel() {
 		setTitle("team08-Fitbit");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1024,768);
@@ -39,19 +46,7 @@ public class StepsPanel extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		/*//create a back button to return you to the dash board
-		JButton btnBack = new JButton("Back");
-		btnBack.setBounds(5, 5, 84, 41);
-		btnBack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				MainScreen main = new MainScreen();
-				main.setVisible(true);
-				dispose();
-			}
-		});
-		btnBack.setHorizontalAlignment(SwingConstants.LEFT);
-		contentPane.add(btnBack);*/
-
+		
 		//create a title for the current pane
 		JLabel lblTitle = new JLabel("STEPS");
 		lblTitle.setFont(new Font("SansSerif", Font.BOLD, 50));
@@ -59,48 +54,33 @@ public class StepsPanel extends JFrame {
 		lblTitle.setBounds(386, 21, 193, 84);
 		contentPane.add(lblTitle);
 
-
-		//ImageIcon BackButton = createImageIcon(backImage, "return to the home page");
+		//create a back button to return to the daily dashboard
 		JLabel imgBack = new JLabel();
 		imgBack.setIcon(new ImageIcon(backImage));
 		imgBack.setBounds(0, 0, 50, 50);
-		imgBack.addMouseListener(new MouseAdapter() {
-   		 @Override
-   		 public void mouseClicked(MouseEvent arg0) {
-   			 //what to do on button click
-   			 MainScreen main = new MainScreen();
-   			 main.setVisible(true);
-   			 dispose();
-   		 }
-   	 });
 		contentPane.add(imgBack);
-		
-		
+
+
 		//create a tabbed pane to store each panel
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(86, 166, 831, 482);
-		
+
 		JPanel pnlToday4 = new JPanel();
 		tabbedPane.addTab("Today's Progress", pnlToday4);
-		
+
 		//create a panel for the Lifetime progress
 		JPanel pnlLifetime = new JPanel();
 		tabbedPane.addTab("lifetime Progress", pnlLifetime);
-		
+
 
 		//create a panel for the Lifetime progress
 		JPanel pnlToday3 = new JPanel();
 		tabbedPane.addTab("Today's Progress", pnlToday3);
-		
+
 		JPanel pnlToday5 = new JPanel();
 		tabbedPane.addTab("Today's Progress", pnlToday5);
-		
+
 		contentPane.add(tabbedPane);
-		
-		
-		/*ImageIcon backButton = new ImageIcon(backImage);
-		JLabel back = new JLabel(backButton);
-		back.setBounds(100, 100, 300, 200);
-		contentPane.add(back);*/
 	}
+
 }
---
+++ src/main/java/ca/uwo/csd/cs2212/team08/StairsPanel.java
@@ -0,0 +1,85 @@
+package ca.uwo.csd.cs2212.team08;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class StairsPanel extends JFrame {
+
+	private JPanel contentPane;
+	private static final String backImage = "src/main/resources/Placeholder.png";
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StairsPanel frame = new StairsPanel();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StairsPanel() {
+		setTitle("team08-Fitbit");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1024,768);
+		this.setLocationRelativeTo(null); 
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		//ImageIcon BackButton = createImageIcon(backImage, "return to the home page");
+		JLabel imgBack = new JLabel();
+		imgBack.setIcon(new ImageIcon(backImage));
+		imgBack.setBounds(0, 0, 50, 50);
+		contentPane.add(imgBack);
+		
+		//create a title for the current pane
+		JLabel lblTitle = new JLabel("STEPS");
+		lblTitle.setFont(new Font("SansSerif", Font.BOLD, 50));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(386, 21, 193, 84);
+		contentPane.add(lblTitle);
+
+
+		//create a tabbed pane to store each panel
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(86, 166, 831, 482);
+
+		JPanel pnlToday4 = new JPanel();
+		tabbedPane.addTab("Today's Progress", pnlToday4);
+
+		//create a panel for the Lifetime progress
+		JPanel pnlLifetime = new JPanel();
+		tabbedPane.addTab("lifetime Progress", pnlLifetime);
+
+
+		//create a panel for the Lifetime progress
+		JPanel pnlToday3 = new JPanel();
+		tabbedPane.addTab("Today's Progress", pnlToday3);
+
+		JPanel pnlToday5 = new JPanel();
+		tabbedPane.addTab("Today's Progress", pnlToday5);
+
+		contentPane.add(tabbedPane);
+	}
+
+}
