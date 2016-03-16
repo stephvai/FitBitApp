3b65ddfd06178830ba844649061d040b536b4381
Dirunyu Wu <dwu232@uwo.ca>
2016-03-16 15:15:32 -0400

    Create the Achievement class

---
+++ src/main/java/ca/uwo/csd/cs2212/team08/Achievement.java
@@ -0,0 +1,23 @@
+package ca.uwo.csd.cs2212.team08;
+
+import java.io.Serializable;
+
+//TODO add images for the accolades
+
+public class Achievement implements Serializable {
+    private String type;
+    private boolean achieved;
+    private float objective;
+
+    public Achievement(){
+        this.achieved = false;
+    }//Constructor.
+    /*Getters*/
+    public boolean getAchieved(){return this. achieved;}
+    public String getType(){return this.type;}
+    public float getObjective(){return this.objective;}
+
+
+
+
+}
