a086b525547184bfd4cd7853e2abb68a11f656d1
Dirunyu Wu <dwu232@uwo.ca>
2016-03-16 19:28:19 -0400

    Add serialversionUID to the class

--- src/main/java/ca/uwo/csd/cs2212/team08/Achievement.java
+++ src/main/java/ca/uwo/csd/cs2212/team08/Achievement.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 public class Achievement implements Serializable {
 
+    private static final long serialVersionUID = -8064307587743333279L;
     private String type;
     private boolean achieved;
     private float objective;
